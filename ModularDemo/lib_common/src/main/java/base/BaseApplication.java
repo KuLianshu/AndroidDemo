7dbac84ac8c6e42e0ca8b33517a26e170feacb09
苦楝树七妹 <2500242213@qq.com>
2020-04-04 16:51:35 +0800

    提交之前写好的Demo

---
+++ ModularDemo/lib_common/src/main/java/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package base;
+
+import android.app.Application;
+
+/**
+ * @author : 韦丽媛
+ * @date : 2019/5/12
+ * Desc :
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ ModularDemo/module_app/src/main/java/com/example/weili/modulardemo/AppApplication.java
@@ -0,0 +1,17 @@
+package com.example.weili.modulardemo;
+
+import base.BaseApplication;
+
+/**
+ * @author : 韦丽媛
+ * @date : 2019/5/12
+ * Desc :
+ */
+
+public class AppApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
