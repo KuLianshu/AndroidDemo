120074a63a2db3829e4c6a7a1071e566503cf0c5
苦楝树七妹 <2500242213@qq.com>
2021-08-25 00:55:09 +0800

    提交解决滑动冲突相关代码

--- TheProgrammeOfArt/DemoOfChapter4/app/src/main/java/com/example/chapter4/widget/CircleView.java
+++ TheProgrammeOfArt/DemoOfChapter4/app/src/main/java/com/example/chapter4/widget/CircleView.java
@@ -20,18 +20,14 @@ public class CircleView extends View {
 
 
     public CircleView(Context context) {
-        super(context);
-        init();
+        this(context,null);
     }
 
 
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
-        mColor = a.getColor(R.styleable.CircleView_circle_color,Color.RED);
-        a.recycle();
-        init();
+        this(context, attrs,0);
+
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -43,10 +39,25 @@ public class CircleView extends View {
     }
 
 
-
     private void init() {
 
         mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200,200);
+        }else if (widthSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200,heightSpecSize);
+        }else if (heightSpecSize == MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthSpecSize,200);
+        }
     }
 
     @Override
@@ -63,4 +74,5 @@ public class CircleView extends View {
         int radius = Math.min(width,height)/2;
         canvas.drawCircle(paddingLeft+width/2,paddingTop+height/2,radius,mPaint);
     }
+
 }
