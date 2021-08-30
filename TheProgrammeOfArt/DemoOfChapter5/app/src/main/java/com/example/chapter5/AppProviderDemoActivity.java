a8cd7e78a142a3f464a85036a503f325c574cc24
苦楝树七妹 <2500242213@qq.com>
2021-08-30 23:33:01 +0800

    添加桌面小部件启动方法

--- TheProgrammeOfArt/DemoOfChapter5/app/src/main/java/com/example/chapter5/AppProviderDemoActivity.java
+++ TheProgrammeOfArt/DemoOfChapter5/app/src/main/java/com/example/chapter5/AppProviderDemoActivity.java
@@ -1,6 +1,7 @@
 package com.example.chapter5;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,5 +12,7 @@ public class AppProviderDemoActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = new Intent("com.ryg.chapter_5.action.CLICK");
+        sendBroadcast(intent);
     }
 }
