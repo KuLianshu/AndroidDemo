ba6ff807c80b6966f05678062fa44e2527371b2b
Code-for-dream <2500242213@qq.com>
2024-03-17 17:54:14 +0800

    提交StateFlow和SharedFlowé¦相关代码

--- LittleDemo/CoroutineDemo/coroutineflow/src/main/java/com/example/coroutineflow/FlowDemoActivity.kt
+++ LittleDemo/CoroutineDemo/coroutineflow/src/main/java/com/example/coroutineflow/FlowDemoActivity.kt
@@ -2,6 +2,7 @@ package com.example.coroutineflow
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
@@ -10,6 +11,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.example.coroutineflow.databinding.ActivityFlowDemoBinding
 import com.example.coroutineflow.model.MainViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -22,22 +24,77 @@ class FlowDemoActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFlowDemoBinding.inflate(layoutInflater)
-        binding.apply {
-            setContentView(root)
-            button.setOnClickListener {
-                //即使页面退到后台，launch{}中的代码依然会继续执行
+        setContentView(binding.root)
+        test3()
+
+    }
+
+//    fun test1(){
+//        binding.apply {
+//            button.setOnClickListener {
+//                //即使页面退到后台，launch{}中的代码依然会继续执行
+////                lifecycleScope.launch{
+//                //页面退到后台，launchWhenStarted{}中的代码不会继续执行，但会为程序保留旧数据，所以重新启动程序，time不会重置为0
+////                lifecycleScope.launchWhenStarted{
 //                lifecycleScope.launch{
-                //页面退到后台，launchWhenStarted{}中的代码不会继续执行，因为此时Activity的状态不是Started
-                lifecycleScope.launchWhenStarted{
-                    mainViewModel.timeFlow.collect{time->
-                        textView.text = time.toString()
-                        Log.d("WLY","Update time $time in UI")
+//                    //页面退到后台，repeatOnLifecycle(Lifecycle.State.STARTED){}中的代码会被完全停止，所以程序重新回到前台，time会被重置为0
+//                    repeatOnLifecycle(Lifecycle.State.STARTED){
+//                        mainViewModel.timeFlow.collect{time->
+//                            textView.text = time.toString()
+//                            Log.d("WLY","Update time $time in UI")
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    fun test2() {
+//        binding.apply {
+//            button.setOnClickListener {
+//                //开启定时器
+//                mainViewModel.startImer()
+//            }
+//            lifecycleScope.launch{
+//                repeatOnLifecycle(Lifecycle.State.STARTED){
+//                    //监听StateFlow
+//                    mainViewModel.stateFlow.collect{time->
+//                        textView.text = time.toString()
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    fun test3() {
+//        lifecycleScope.launch{
+//            //这里repeatOnLifecycle(Lifecycle.State.STARTED){}不能缺，否则程序退到后台，flow还是会继续执行
+//            repeatOnLifecycle(Lifecycle.State.STARTED){
+//                mainViewModel.stateFlow.collect{time->
+//                    binding.textView.text = time.toString()
+//                    Log.i("WLY","time: $time")
+//                }
+//            }
+//        }
+//    }
+
+
+    fun test3() {
+        binding.apply {
+            button.setOnClickListener {
+                mainViewModel.startLogin()
+            }
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED){
+                    //因为SharedFlow是非粘性的，所以横竖屏切换之后，loginFlow的值会重置为null
+                    mainViewModel.loginFlow.collect{
+                        if (it.isNotBlank()){
+                            Toast.makeText(this@FlowDemoActivity,it,Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
         }
-
-
     }
 
 }
--- LittleDemo/CoroutineDemo/coroutineflow/src/main/java/com/example/coroutineflow/model/MainViewModel.kt
+++ LittleDemo/CoroutineDemo/coroutineflow/src/main/java/com/example/coroutineflow/model/MainViewModel.kt
@@ -1,16 +1,64 @@
 package com.example.coroutineflow.model
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.flow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import java.util.Timer
+import java.util.TimerTask
 
 //创建Flow
+//class MainViewModel: ViewModel() {
+//    val timeFlow = flow {
+//        var time = 0
+//        while (true){
+//            emit(time)
+//            kotlinx.coroutines.delay(1000)
+//            time++
+//        }
+//    }
+//}
+
+
+//创建StateFlow
+//class MainViewModel: ViewModel() {
+//    private val _stateFlow = MutableStateFlow(0)
+//    val stateFlow = _stateFlow.asStateFlow()
+//    fun startImer(){
+//        val timer = Timer()
+//        timer.scheduleAtFixedRate(object : TimerTask(){
+//            override fun run() {
+//                _stateFlow.value += 1
+//            }
+//        },0,1000)
+//    }
+//}
+
+
+//class MainViewModel: ViewModel() {
+//    private val timeFlow = flow {
+//        var time = 0
+//        while (true){
+//            emit(time)
+//            delay(1000)
+//            time++
+//        }
+//    }
+//    val stateFlow = timeFlow.stateIn(
+//        viewModelScope,
+//        //那么只要在5秒钟内横竖屏切换完成了，Flow就不会停止工作
+//        SharingStarted.WhileSubscribed(5000),
+//        0)
+//}
+
+//创建SharedFlow
 class MainViewModel: ViewModel() {
-    val timeFlow = flow {
-        var time = 0
-        while (true){
-            emit(time)
-            kotlinx.coroutines.delay(1000)
-            time++
+    private val _loginFlow = MutableSharedFlow<String>()
+    val loginFlow = _loginFlow.asSharedFlow()
+    fun startLogin(){
+        viewModelScope.launch {
+            _loginFlow.emit("Login Success")
         }
     }
 }
